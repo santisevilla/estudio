9316a113722e45fdb42c5e3c92875f86e1b9803f
Santiago <santisevilla.9@gmail.com>
2023-11-20 20:41:11 -0300

    asd

--- ejerciciosreact/src/index.js
+++ ejerciciosreact/src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+    </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part} {props.exercise}</p>
+    </div>
+    )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>Number of exercises {props.sum}</p>
+    </div>
+    )
+}
+
+const App = () => {
+  return (
+    <div>
+      <Header name="Half Stack application development"/>
+      <Content part="Fundamentals of React" exercise={10}/>
+      <Content part="Using props to pass data" exercise={7}/>
+      <Content part="State of a component" exercise={14}/>
+      <Total sum={10 + 7 + 14}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
