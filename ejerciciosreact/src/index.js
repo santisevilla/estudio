3354cc0c3ba165a9a38473b12792ae8c1b5f8d61
Santiago <santisevilla.9@gmail.com>
2023-11-21 16:16:46 -0300

    asd

--- ejerciciosreact/src/index.js
+++ ejerciciosreact/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 // const Header = (props) => {
@@ -45,31 +45,57 @@ import ReactDOM from 'react-dom'
 //   )
 // }
 
-const Hello = ({name, age}) => {
-  const bornYear = () => new Date().getFullYear() - age
+// const Hello = ({name, age}) => {
+//   const bornYear = () => new Date().getFullYear() - age
 
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
+//   return (
+//     <div>
+//       <p>
+//         Hello {name}, you are {age} years old
+//       </p>
 
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
+//       <p>So you were probably born in {bornYear()}</p>
+//     </div>
+//   )
+// }
+
+// const App = () => {
+//   const name = 'Peter'
+//   const age = 10
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Maya" age={26 + 10} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   )
+// }
 
 const App = () => {
-  const name = 'Peter'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+
+
+  const increaseByOne = () => setCounter(counter + 1)
+  
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
+      <div>{counter}</div>
+
+      <button onClick={increaseByOne}>
+        plus
+      </button>
+
+      <button onClick={setToZero}>
+        zero
+      </button>
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
